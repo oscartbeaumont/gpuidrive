49cd745c8b3c528088b070f81629fa78448e0bac
Oscar Beaumont <oscar@otbeaumont.me>
2025-04-25 12:26:02 -0700

    model file information + fetch it on path change

--- gpuidrive/src/state.rs
+++ gpuidrive/src/state.rs
@@ -1,17 +1,51 @@
-use std::path::PathBuf;
+use std::{ffi::OsString, fs::FileType, os::unix::fs::MetadataExt, path::PathBuf};
+
+use chrono::{DateTime, Local};
 
 pub struct State {
     path: PathBuf,
-    // TODO: make this reactive to `path` changing
-    files: Vec<String>,
+    nodes: Vec<Node>,
+}
+
+/// Represents a node on the filesystem.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Node {
+    path: PathBuf,
+    name: OsString,
+    kind: NodeKind,
+    size: u64,
+    created: DateTime<Local>,
+    modified: DateTime<Local>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum NodeKind {
+    File,
+    Directory,
+    Unknown, // TODO: Removing this
+             // TODO: Handle symbolic links, etc
+}
+
+impl From<FileType> for NodeKind {
+    fn from(value: FileType) -> Self {
+        if value.is_dir() {
+            NodeKind::Directory
+        } else if value.is_file() {
+            NodeKind::File
+        } else {
+            NodeKind::Unknown
+        }
+    }
 }
 
 impl State {
     pub fn init() -> Self {
-        Self {
-            path: "/Users/oscar/Desktop".into(), // TODO: Don't hardcode
-            files: vec![],
-        }
+        let mut this = Self {
+            path: Default::default(),
+            nodes: Default::default(),
+        };
+        this.set_path(PathBuf::from("/Users/oscar/Desktop")); // TODO: Don't hardcode username
+        this
     }
     pub fn path(&self) -> &PathBuf {
         &self.path
@@ -22,11 +56,30 @@ impl State {
         self.path = path;
 
         if changed {
-            // TODO: Error handing + run off the main thread
-            self.files = std::fs::read_dir(&self.path)
-                .unwrap()
-                .map(|entry| entry.unwrap().file_name().into_string().unwrap())
-                .collect();
+            match std::fs::read_dir(&self.path) {
+                Ok(dir) => {
+                    // TODO: Error handing
+                    // TODO: Is this running off the main thread?
+                    self.nodes = dir
+                        .map(|entry| {
+                            let entry = entry.unwrap();
+                            let metadata = entry.metadata().unwrap();
+
+                            Node {
+                                path: entry.path(),
+                                name: entry.file_name(),
+                                kind: entry.file_type().unwrap().into(),
+                                size: metadata.size(),
+                                created: metadata.created().unwrap().into(),
+                                modified: metadata.modified().unwrap().into(),
+                            }
+                        })
+                        .collect();
+
+                    println!("{:?}", self.nodes); // TODO
+                }
+                Err(_) => self.nodes = vec![], // TODO: Proper error handling
+            }
         }
     }
 }
